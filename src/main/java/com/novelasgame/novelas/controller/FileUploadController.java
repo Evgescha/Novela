4a2cae73292146fd2b2f44ca6fe91e4903d0f608
Evgescha <evgescha.maslov007@yandex.ru>
2020-03-16 01:18:51 +0300

    added work variant

--- src/main/java/com/novelasgame/novelas/controller/FileUploadController.java
+++ src/main/java/com/novelasgame/novelas/controller/FileUploadController.java
@@ -1,6 +1,7 @@
 package com.novelasgame.novelas.controller;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,9 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.novelasgame.novelas.entity.DataBase.Game;
+import com.novelasgame.novelas.service.DataBase.GameService;
+import com.novelasgame.novelas.service.DataBase.ResourcesItemService;
 import com.novelasgame.novelas.storage.StorageFileNotFoundException;
 import com.novelasgame.novelas.storage.StorageService;
 
@@ -26,6 +30,10 @@ import com.novelasgame.novelas.storage.StorageService;
 public class FileUploadController {
 
 	private final StorageService storageService;
+	@Autowired
+	GameService gameService;
+	@Autowired
+	ResourcesItemService resourcesItemService;
 
 	@Autowired
 	public FileUploadController(StorageService storageService) {
@@ -34,6 +42,8 @@ public class FileUploadController {
 
 	@GetMapping("/upload")
 	public String listUploadedFiles(Model model) throws IOException {
+		List<Game> findAll = gameService.findAll();
+		model.addAttribute("games", findAll);
 
 		model.addAttribute("files",
 				storageService.loadAll()
@@ -50,14 +60,14 @@ public class FileUploadController {
 	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
 
 		Resource file = storageService.loadAsResource(filename);
-		return ResponseEntity.ok()
-				.header(HttpHeaders.CONTENT_DISPOSITION)
-				.body(file);
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION).body(file);
 	}
 
-	@PostMapping("/")
-	public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
-
+	@PostMapping("/upload")
+	public String handleFileUpload(@RequestParam("file") MultipartFile file, @RequestParam("game") String game,
+			@RequestParam("type") String type, RedirectAttributes redirectAttributes) {
+		System.out.println("game is: " + game);
+		System.out.println("type is: " + type);
 		storageService.store(file);
 		redirectAttributes.addFlashAttribute("message",
 				"You successfully uploaded " + file.getOriginalFilename() + "!");

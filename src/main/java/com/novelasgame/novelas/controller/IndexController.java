67895ce4eb93657f892e67624581139d2bdf9125
Evgescha <evgescha.maslov007@yandex.ru>
2020-03-19 01:06:34 +0300

    added start page (skelet)

--- src/main/java/com/novelasgame/novelas/controller/IndexController.java
+++ src/main/java/com/novelasgame/novelas/controller/IndexController.java
@@ -1,16 +1,30 @@
 package com.novelasgame.novelas.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.novelasgame.novelas.entity.DataBase.Game;
+import com.novelasgame.novelas.service.DataBase.GameService;
+
 @Controller
 @RequestMapping("/")
 public class IndexController {
-    @GetMapping()
-    private String one(Model model) {     
-        System.out.println("view index scene");
-        return "index";
-    }
+	@Autowired
+	GameService gameService;
+
+	@GetMapping()
+	private String one(Model model) {
+		System.out.println("view index scene");
+
+		List<Game> games = gameService.findAll();
+		if (!games.isEmpty())
+			model.addAttribute("games", games);
+		
+		return "index2";
+	}
 }

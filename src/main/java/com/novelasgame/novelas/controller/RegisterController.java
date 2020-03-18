32aafbf7ab547cbcd0cf664bbb5f9ab6e8b5967d
Evgescha <evgescha.maslov007@yandex.ru>
2020-03-19 02:53:54 +0300

    add responce about succesed or failed registraion

--- src/main/java/com/novelasgame/novelas/controller/RegisterController.java
+++ src/main/java/com/novelasgame/novelas/controller/RegisterController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.novelasgame.novelas.entity.DataBase.User;
 import com.novelasgame.novelas.service.DataBase.UserServiceImpl;
@@ -18,18 +19,18 @@ public class RegisterController {
     @Autowired
     UserServiceImpl userService;
 
-    @GetMapping
-    public String getRegistration() {
-        return "login";
-    }
+//    @GetMapping
+//    public String getRegistration() {
+//        return "login";
+//    }
     
     @PostMapping
-    public String setRegistrationForm(Model model, @ModelAttribute User user) {
+    public String setRegistrationForm(Model model, @ModelAttribute User user,RedirectAttributes redirectAttributes) {
         System.out.println("start registration");
         boolean success = userService.userRegistration(user);
         String response = success ? "Success registration" : "Registration failed";
         System.out.println("registration users: "+response);
-        model.addAttribute("success", response);
-        return "index";
+        redirectAttributes.addFlashAttribute("success", response);
+        return "redirect:/";
     }
 }

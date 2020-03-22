840039cbf4c4182f3d37b3e261c34651fcb59b6b
Evgescha <evgescha.maslov007@yandex.ru>
2020-03-23 02:44:56 +0300

    add new entity Genre

---
+++ src/main/java/com/novelasgame/novelas/entity/DataBase/Genre.java
@@ -0,0 +1,35 @@
+package com.novelasgame.novelas.entity.DataBase;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Table
+@Entity
+public class Genre extends AbstractEntity {
+    
+    @Column(unique = true)
+    private String name;
+    
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.EAGER)
+    private Collection<User> users = new ArrayList<User>();
+
+	public Genre(String name) {
+		super();
+		this.name = name;
+	}
+
+	public Genre() {
+		super();
+	}
+
+    
+}

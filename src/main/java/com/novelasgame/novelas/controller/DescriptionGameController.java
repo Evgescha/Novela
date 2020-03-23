62d4b95355336156afaccc1f1f5c12e3b39508fc
Evgescha <evgescha.maslov007@yandex.ru>
2020-03-23 03:01:09 +0300

    added some work skeleton for description page

--- src/main/java/com/novelasgame/novelas/controller/DescriptionGameController.java
+++ src/main/java/com/novelasgame/novelas/controller/DescriptionGameController.java
@@ -1,16 +1,27 @@
 package com.novelasgame.novelas.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.novelasgame.novelas.service.DataBase.GameService;
 
 @Controller
 @RequestMapping("/description")
 public class DescriptionGameController {
 
-	@GetMapping
-	public String getDescriptionGamePage(Model model){
+	
+	@Autowired
+	GameService gameService;
+	
+	
+	@GetMapping("/{gameId}")
+	public String getDescriptionGamePage(@PathVariable long gameId, Model model){
+		
+		model.addAttribute("game", gameService.read(gameId));
 		return "descriptionGame";
 	}
 }

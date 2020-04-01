181b85caa070ff6eec96bacc8148557d527b7b99
Evgescha <evgescha.maslov007@yandex.ru>
2020-04-01 20:29:38 +0300

    jump work now

--- src/main/webapp/assets/js/cmdJump.js
+++ src/main/webapp/assets/js/cmdJump.js
@@ -1,5 +1,5 @@
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!JUMP
 function jump(jmp){
 	console.log("Go to label: "+jmp["label"]);
-	document.location.href ="/runGame?gameName="+jmp["gameName"]+"&labelName="+jmp["label"];
+	document.location.href ="/runGame?gameId="+jmp["gameId"]+"&labelName="+jmp["label"];
 }

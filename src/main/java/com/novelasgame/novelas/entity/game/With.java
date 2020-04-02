241fad02af4902911e3a70d9e4660554d929220d
Evgescha <evgescha.maslov007@yandex.ru>
2020-04-02 17:49:25 +0300

    centering text

--- src/main/java/com/novelasgame/novelas/entity/game/With.java
+++ src/main/java/com/novelasgame/novelas/entity/game/With.java
@@ -8,15 +8,18 @@ public class With {
 	private final String type = "with";
 	private String effect="";
 	/*
-	 *  with dspr - показать плавно, за 0.2 секунды
+	    with dspr - показать плавно, за 0.2 секунды
 		with dissolve - за 1 сек
 		with dissolve2 - за 2 сек
 		with fade - показать с затемнением, за 1 секунду
-		with fade2 - 2 сек with flash - 2 сек (fade to white)
-		with fade3 - 3 сек with fiash_red - 2 сек (fade to red)
-		with hpunch - трясёт экран по горизонтали with vpunch - трясёт экран по вертикали
+		with fade2 - 2 сек 
+		with fade3 - 3 сек 
+		with flash - 2 сек (fade to white)
+		with fiash_red - 2 сек (fade to red)
+		with hpunch - трясёт экран по горизонтали 
+		with vpunch - трясёт экран по вертикали
 		Не применяется к конкретному show/scene.
-	 * */
+	*/
 	
 	public With() {
 	}
